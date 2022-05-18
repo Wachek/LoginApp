10f2d70526db453584adc03bc16b28938776e40f
Viacheslav Turchak <wachek7@gmail.com>
2022-05-18 22:07:59 +0300

    setup buttons on login screen

--- LoginApp/ViewController.swift
+++ LoginApp/ViewController.swift
@@ -9,11 +9,55 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var userNameTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func loginButtonPressed() {
+        
+        textFormatCheck(for: userNameTextField.text)
+        textFormatCheck(for: passwordTextField.text)
+        
+        guard userNameTextField.text == "User" else {
+            showAlert(with: "Wrong user name or password", and: "Please, enter correct user name and password")
+            return
+        }
+        
+        guard passwordTextField.text == "Password" else {
+            showAlert(with: "Wrong user name or password", and: "Please, enter correct user name and password")
+            return
+        }
+    }
+    
+    @IBAction func forgotNameButtonPressed() {
+        showAlert(with: "Uuups!", and: "Your user name is User")
+    }
+    
+    @IBAction func forgotPasswordButtonPressed() {
+        showAlert(with: "Uuups!", and: "Your password is Password")
+    }
+    
 }
 
+extension ViewController {
+    
+    private func showAlert(with title: String, and massage: String) {
+        let alert = UIAlertController(title: title, message: massage, preferredStyle: .alert)
+        let returnAction = UIAlertAction(title: "Return", style: .cancel) { _ in
+            self.passwordTextField.text = ""
+        }
+        alert.addAction(returnAction)
+        present(alert, animated: true)
+    }
+    
+    private func textFormatCheck (for text: String?) {
+        guard let inputText = text, !inputText.isEmpty else {
+            showAlert(with: "Text field is empty", and: "Please, enter user name and password")
+            return
+        }
+    }
+}
